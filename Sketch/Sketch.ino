5a6a5388afec10a72891c488906c77d45c00a548
Timo Denk <simssosdenk@googlemail.com>
2016-04-24 19:34:28 +0200

    Bugfixes and reset

--- Sketch/Sketch.ino
+++ Sketch/Sketch.ino
@@ -2,12 +2,15 @@
 
 #define TASTE_1 2
 #define TASTE_2 4
-#define Taste_3 7
+#define TASTE_3 7
+#define TASTE_4 8
+#define TASTE_7 12
+
 #define DREHGEBER_1 A0
 #define DREHGEBER_2 A1
 #define DREHGEBER_3 A2
 
-#define SERVO_1_PIN 12
+#define SERVO_1_PIN 3
 #define SERVO_2_PIN 5
 #define SERVO_3_PIN 6
 #define SERVO_4_PIN 9
@@ -21,13 +24,15 @@
 class CustomServo : public Servo
 {
 public:
-  CustomServo(unsigned pin, unsigned analogInputPin, unsigned* activePins, unsigned numActivePins) : Servo() {
+  CustomServo(unsigned pin, unsigned analogInputPin, unsigned* highPins, unsigned numHighPins, unsigned* lowPins, unsigned numLowPins) : Servo() {
     Servo::attach(pin);
     pinMode(pin, OUTPUT);
     pinMode(analogInputPin, INPUT);
     _pin = pin;
-    _activePins = activePins;
-    _numActivePins = numActivePins;
+    _highPins = highPins;
+    _numHighPins = numHighPins;
+    _lowPins = lowPins;
+    _numLowPins = numLowPins;
     _analogInputPin = analogInputPin;
   }
 
@@ -46,8 +51,12 @@ private:
   bool _init = true;
   unsigned _pin;
   unsigned _analogInputPin;
-  unsigned* _activePins;
-  unsigned _numActivePins;
+  
+  unsigned* _highPins;
+  unsigned _numHighPins;
+  unsigned* _lowPins;
+  unsigned _numLowPins;
+  
   unsigned _lastValues[AVERAGE_SAMPLES_NUM];
   unsigned _lastValuesPtr = 0;
 
@@ -60,11 +69,20 @@ private:
   };
 
   bool isActive() {
-    for (unsigned i = 0; i < _numActivePins; i++) {
-      if (!digitalRead(_activePins[i])) {
+    // check high high pins
+    for (unsigned i = 0; i < _numHighPins; i++) {
+      if (!digitalRead(_highPins[i])) {
         return false;
       }
     }
+
+    // check low pins
+    for (unsigned i = 0; i < _numLowPins; i++) {
+      if (digitalRead(_lowPins[i])) {
+        return false;
+      }
+    }
+    
     return true;
   };
   
@@ -89,6 +107,11 @@ private:
 };
 
 
+void softwareReset() {
+  asm volatile ("jmp 0"); // reset software
+}
+
+
 void setup() {
   // serial connection for debugging
   Serial.begin(9600);
@@ -98,34 +121,38 @@ void setup() {
   // initialize servos
 
   // servo 1
-  unsigned servo1ActivePins[] = { TASTE_1 };
-  CustomServo servo1(SERVO_1_PIN, DREHGEBER_1, servo1ActivePins, sizeof(servo1ActivePins) / sizeof(unsigned));
+  unsigned servo1HighPins[] = { TASTE_1 }, servo1LowPins[] = { TASTE_2 };
+  CustomServo servo1(SERVO_1_PIN, DREHGEBER_1, servo1HighPins, sizeof(servo1HighPins) / sizeof(unsigned), servo1LowPins, sizeof(servo1LowPins));
 
   // servo 2
-  unsigned servo2ActivePins[] = { TASTE_1 };
-  CustomServo servo2(SERVO_2_PIN, DREHGEBER_2, servo2ActivePins, sizeof(servo2ActivePins) / sizeof(unsigned));
+  unsigned servo2HighPins[] = { TASTE_1 }, servo2LowPins[] = { TASTE_2 };
+  CustomServo servo2(SERVO_2_PIN, DREHGEBER_2, servo2HighPins, sizeof(servo2HighPins) / sizeof(unsigned), servo2LowPins, sizeof(servo2LowPins));
 
   // servo 3
-  unsigned servo3ActivePins[] = { TASTE_1 };
-  CustomServo servo3(SERVO_3_PIN, DREHGEBER_3, servo3ActivePins, sizeof(servo3ActivePins) / sizeof(unsigned));
+  unsigned servo3HighPins[] = { TASTE_1 }, servo3LowPins[] = { TASTE_2 };
+  CustomServo servo3(SERVO_3_PIN, DREHGEBER_3, servo3HighPins, sizeof(servo3HighPins) / sizeof(unsigned), servo3LowPins, sizeof(servo3LowPins));
 
   // servo 4
-  unsigned servo4ActivePins[] = { TASTE_1, TASTE_2 };
-  CustomServo servo4(SERVO_4_PIN, DREHGEBER_1, servo4ActivePins, sizeof(servo4ActivePins) / sizeof(unsigned));
+  unsigned servo4HighPins[] = { TASTE_1, TASTE_2 }, servo4LowPins[] = {};
+  CustomServo servo4(SERVO_4_PIN, DREHGEBER_1, servo4HighPins, sizeof(servo4HighPins) / sizeof(unsigned), servo4LowPins, sizeof(servo4LowPins));
 
   // servo 5
-  unsigned servo5ActivePins[] = { TASTE_1, TASTE_2 };
-  CustomServo servo5(SERVO_5_PIN, DREHGEBER_2, servo5ActivePins, sizeof(servo5ActivePins) / sizeof(unsigned));
+  unsigned servo5HighPins[] = { TASTE_1, TASTE_2 }, servo5LowPins[] = {};
+  CustomServo servo5(SERVO_5_PIN, DREHGEBER_2, servo5HighPins, sizeof(servo5HighPins) / sizeof(unsigned), servo5LowPins, sizeof(servo5LowPins));
 
   // servo 6
-  unsigned servo6ActivePins[] = { TASTE_1, TASTE_2 };
-  CustomServo servo6(SERVO_6_PIN, DREHGEBER_3, servo6ActivePins, sizeof(servo6ActivePins) / sizeof(unsigned));
+  unsigned servo6HighPins[] = { TASTE_1, TASTE_2 }, servo6LowPins[] = {};
+  CustomServo servo6(SERVO_6_PIN, DREHGEBER_3, servo6HighPins, sizeof(servo6HighPins) / sizeof(unsigned), servo6LowPins, sizeof(servo6LowPins));
 
   CustomServo servos[NUM_SERVOS] = { servo1, servo2, servo3, servo4, servo5, servo6 };
 
   while (true) {
     for (unsigned i = 0; i < NUM_SERVOS; i++) {
       servos[i].update();
+    }
+
+    if (digitalRead(TASTE_4) && digitalRead(TASTE_7)) {
+      softwareReset();
     }
   }
 }
